49636501b03d52725d746af90886a5daa7d1694e
12ahnaf34 <72951659+12ahnaf34@users.noreply.github.com>
2022-09-29 12:22:22 -0400

    Update Airtable.js
    Removed unused imports, added squiggly brackets to return statements.

--- src/api/Airtable.js
+++ src/api/Airtable.js
@@ -1,6 +1,3 @@
-import Airtable from "airtable";
-import React from "react";
-
 function fetchTodo() {
   console.log("Fetch");
 }
@@ -13,9 +10,9 @@ function initialFetchList(setList, setLoading) {
     .then((response) => response.json())
     .then((result) => {
       const sortedList = result.records.sort((objectA, objectB) => {
-        if (objectA.fields.Title < objectB.fields.Title) return 1;
-        if (objectA.fields.Title === objectB.fields.Title) return 0;
-        if (objectA.fields.Title > objectB.fields.Title) return -1;
+        if (objectA.fields.Title < objectB.fields.Title) {return 1};
+        if (objectA.fields.Title === objectB.fields.Title) {return 0};
+        if (objectA.fields.Title > objectB.fields.Title) {return -1};
       });
       setList(sortedList);
       setLoading(false);
@@ -36,9 +33,9 @@ function createTodo(todoTitle, list, setList) {
       const item = result.records[0];
       let newList = [...list, item];
       const sortedList = newList.sort((objectA, objectB) => {
-        if (objectA.fields.Title < objectB.fields.Title) return 1;
-        if (objectA.fields.Title === objectB.fields.Title) return 0;
-        if (objectA.fields.Title > objectB.fields.Title) return -1;
+        if (objectA.fields.Title < objectB.fields.Title) {return 1};
+        if (objectA.fields.Title === objectB.fields.Title) {return 0};
+        if (objectA.fields.Title > objectB.fields.Title) {return -1};
       });
       setList(sortedList);
     });
